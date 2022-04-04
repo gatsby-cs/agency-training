e070f527ea893c4f5a26daf9d14186406dfc0b6f
Grayson Hicks <graysonhicks@gmail.com>
2022-04-04 13:56:37 -0400

    single sprite image

---
+++ src/pages/svg-sprite.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SpriteIcon } from '../components/icon/icon'
+
+function SvgSprite() {
+  return (
+    <h3>
+      <SpriteIcon key="airbnb" iconName={`github`} />
+    </h3>
+  )
+}
+
+export default SvgSprite
