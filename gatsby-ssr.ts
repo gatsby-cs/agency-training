19f0ad02d99d690b2f65d9bcfb034acfc1326a97
Grayson Hicks <graysonhicks@gmail.com>
2022-04-05 16:30:50 -0400

    test jquery load

--- gatsby-ssr.ts
+++ gatsby-ssr.ts
@@ -1,2 +1,12 @@
 // import wrapWithProvider from './src/state/wrap-with-provider'
 // export const wrapRootElement = wrapWithProvider
+
+export const onPreRenderHTML = ({
+  getHeadComponents,
+  replaceHeadComponents,
+}) => {
+  replaceHeadComponents([
+    ...getHeadComponents,
+    '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>',
+  ])
+}
