748b7e0fce66b63c59d58d34492d213f6355bf3e
Grayson Hicks <graysonhicks@gmail.com>
2022-04-05 17:20:07 -0400

    browser script loads

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,2 +1,17 @@
 // import wrapWithProvider from './src/state/wrap-with-provider'
 // export const wrapRootElement = wrapWithProvider
+const addScript = (url) => {
+  const script = document.createElement('script')
+  script.src = url
+  script.async = true
+  document.body.appendChild(script)
+}
+export const onClientEntry = () => {
+  console.log('OnClientEntry!')
+  addScript('https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js')
+}
+
+export const onInitialClientRender = () => {
+  console.log('OnInitialClientRender!')
+  addScript('https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js')
+}
