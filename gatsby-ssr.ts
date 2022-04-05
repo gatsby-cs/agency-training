16e990753a8eb7a807c57481c8579bebb6fbb6df
Grayson Hicks <graysonhicks@gmail.com>
2022-04-05 16:39:39 -0400

    add script w/o quotes

--- gatsby-ssr.ts
+++ gatsby-ssr.ts
@@ -7,6 +7,6 @@ export const onPreRenderHTML = ({
 }) => {
   replaceHeadComponents([
     ...getHeadComponents(),
-    '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>',
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>,
   ])
 }
