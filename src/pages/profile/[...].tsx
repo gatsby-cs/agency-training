f7b31d3ca10d4ee1ab3345a11fc7aec7619d5f0b
Grayson Hicks <graysonhicks@gmail.com>
2022-04-05 22:12:51 -0400

    profile styles

--- src/pages/profile/[...].tsx
+++ src/pages/profile/[...].tsx
@@ -12,20 +12,65 @@ import { withServerSideAuth } from '@clerk/nextjs/ssr'
 import Layout from '../../components/layout/layout'
 
 import * as styles from '../../styles/pages/profile.module.scss'
+import { userInfo } from 'os'
 type ClientPageProps = {
   path: string
 }
 
-const Details: React.FC<ClientPageProps> = (props) => {
-  return <h2>Details!</h2>
+type DefaultPageProps = {
+  path: string
+  fullName?: string | null
+  avatar?: string
+  email?: string
+}
+
+type DetailsPageProps = {
+  path: string
+  dateCreated?: Date | null
+  passwordEnabled?: boolean
+}
+
+const Default: React.FC<DefaultPageProps> = ({ fullName, avatar, email }) => {
+  return (
+    <>
+      <h2>Profile</h2>
+      <img src={avatar} className={styles.avatar} />
+      <p>Full Name: {fullName}</p>
+      <p>Email: {email}</p>
+    </>
+  )
+}
+
+const Details: React.FC<DetailsPageProps> = ({
+  dateCreated,
+  passwordEnabled,
+}) => {
+  return (
+    <>
+      <h2>Details</h2>
+      <p>Date created: {dateCreated?.toLocaleDateString('en-US')}</p>
+      <p>Password enabled: {passwordEnabled ? 'true' : 'false'}</p>
+    </>
+  )
 }
 
 const Account: React.FC<ClientPageProps> = (props) => {
-  return <h2>Account!</h2>
+  return (
+    <>
+      <h2>Account</h2>
+      <p>
+        <button className={styles.button}>Reset password</button>
+      </p>
+      <p>
+        <button className={styles.button}>Change email</button>
+      </p>
+    </>
+  )
 }
 
 export default function Profile({ serverData }) {
   const user = useUser()
+  console.log(user)
 
   return (
     <>
@@ -35,12 +80,28 @@ export default function Profile({ serverData }) {
       <SignedIn>
         <Layout>
           <div className={styles.container}>
-            <h1>Profile!</h1>
-            <Link to="/profile/">Profile Page</Link>
-            <Link to="/profile/details">Details Page</Link>
-            <Link to="/profile/account">Account Page</Link>
+            <h1>Profile</h1>
+            <Link className={styles.link} to="/profile/">
+              Profile Page
+            </Link>
+            <Link className={styles.link} to="/profile/details">
+              Details Page
+            </Link>
+            <Link className={styles.link} to="/profile/account">
+              Account Page
+            </Link>
             <Router basepath="/profile">
-              <Details path="/details" />
+              <Default
+                path="/"
+                fullName={user.user?.fullName}
+                avatar={user.user?.profileImageUrl}
+                email={user.user?.primaryEmailAddress?.emailAddress}
+              />
+              <Details
+                path="/details"
+                dateCreated={user.user?.createdAt}
+                passwordEnabled={user.user?.passwordEnabled}
+              />
               <Account path="/account" />
             </Router>
           </div>
