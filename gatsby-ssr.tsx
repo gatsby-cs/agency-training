8e20cb81d06ad46b625c97db8d0a6d243ec66d6b
Grayson Hicks <graysonhicks@gmail.com>
2022-10-17 16:19:12 -0500

    preconnects

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -13,5 +13,7 @@ export const onPreRenderHTML = ({
       key="jquery"
       src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"
     />,
+    <link rel="preconnect" href="https://fonts.googleapis.com" />,
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />,
   ])
 }
